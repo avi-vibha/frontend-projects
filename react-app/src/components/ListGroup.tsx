b0ebad7730c6cf485e2c30cba6d8fd0afd10741d
avi-vibha <avi.vib@protonmail.com>
2025-04-16 21:13:22 +0530

    01. Conditional Rendering

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,16 +1,28 @@
 import { Fragment } from "react";
 
 function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
 
-  const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris']; 
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No list items</p>
+      </>
+    );
 
   return (
-   <>
-    <h1>List</h1>
-    <ul className="list-group">
-        {items.map(item=><li className="list-group-item" key={item}>{item}</li>)}
-    </ul>
-   </>
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 
   // the above errors are because 'class' is a reserved keyword
@@ -18,14 +30,14 @@ function ListGroup() {
 
   // Ctrl + D for multiple selection
 
-  // Ctrl + Shift + P : format document using prettier 
+  // Ctrl + Shift + P : format document using prettier
   // Prettier automatically wrapped our jsx markup in parantheses. This is necessary to break the markup in multiple lines
 
   // No for loop in jsx
 
   // map method is used to take elements from an array and convert it to a different type
 
-  // CHECK tutorial-points.odt in public folder. 
+  // CHECK tutorial-points.odt in public folder.
 }
 
 export default ListGroup;
