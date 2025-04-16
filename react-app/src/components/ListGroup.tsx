35271d7644eb15faa42101e782e8c5312f773e9a
avi-vibha <avi.vib@protonmail.com>
2025-04-16 12:11:50 +0530

    02. Creating a list group component. class keyword error

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,5 +1,15 @@
 function ListGroup () {
-    return <h1>List Group</h1>;
+    return 
+    <ul class="list-group">
+        <li class="list-group-item">An item</li>
+        <li class="list-group-item">A second item</li>
+        <li class="list-group-item">A third item</li>
+        <li class="list-group-item">A fourth item</li>
+        <li class="list-group-item">And a fifth one</li>
+    </ul>;
+
+    // the above errors are because 'class' is a reserved keyword
+    // in js or ts, so we need to rename all of them to 'className'
 }
 
 export default ListGroup;
