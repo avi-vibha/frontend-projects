338650ce9f7de79721edd96753461e9a7a0a57f4
avi-vibha <avi.vib@protonmail.com>
2025-04-16 12:24:57 +0530

    03. Creating a list group component

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,15 +1,21 @@
-function ListGroup () {
-    return 
-    <ul class="list-group">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item">A second item</li>
-        <li class="list-group-item">A third item</li>
-        <li class="list-group-item">A fourth item</li>
-        <li class="list-group-item">And a fifth one</li>
-    </ul>;
+function ListGroup() {
+  return (
+    <ul className="list-group">
+      <li className="list-group-item">An item</li>
+      <li className="list-group-item">A second item</li>
+      <li className="list-group-item">A third item</li>
+      <li className="list-group-item">A fourth item</li>
+      <li className="list-group-item">And a fifth one</li>
+    </ul>
+  );
 
-    // the above errors are because 'class' is a reserved keyword
-    // in js or ts, so we need to rename all of them to 'className'
+  // the above errors are because 'class' is a reserved keyword
+  // in js or ts, so we need to rename all of them to 'className'
+
+  // Ctrl + D for multiple selection
+
+  // Ctrl + Shift + P : format document using prettier 
+  // Prettier automatically wrapped our jsx markup in parantheses. This is necessary to break the markup in multiple lines
 }
 
 export default ListGroup;
